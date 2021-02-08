835f6ecaad4faa923265f27d0bd2370ba91f4bfb
Zoran Gasic <zoran.gasic@cleoo.com>
2021-02-08 09:39:35 +0100

    update composer loader

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -1,11 +1,18 @@
 <?php
 declare(strict_types=1);
+namespace Tests;
+
+use App\App;
+use App\Request;
 use PHPUnit\Framework\TestCase;
 
 final class AppTest extends TestCase
 {
     public function testAppExists()
     {
+        $request = new Request();
+
         $app = new App();
+        $app->run($request);
     }
 }
