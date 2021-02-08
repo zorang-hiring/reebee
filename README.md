c76d7a627f3bde858fbc469310c224888501ee87
Zoran Gasic <zoran.gasic@cleoo.com>
2021-02-08 09:29:10 +0100

    init tests

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # reebee
+
+## Tests
+
+PHP version > 7.1 needed. Run:
+
+'''
+composer test
+
+'''
---
+++ tests/AppTest.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class AppTest extends TestCase
+{
+    public function testAppExists()
+    {
+        $app = new App();
+    }
+}
